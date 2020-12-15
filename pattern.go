fae5c6f5031ffec51a8963a912a141b7e876d33b
longfengpili <398745129@qq.com>
2020-12-15 14:06:08 +0800

    [新增]pattern

---
+++ pattern.go
@@ -0,0 +1,36 @@
+/*
+* @Author: chunyang.xu
+* @Date:   2020-12-14 18:00:55
+* @Last Modified by:   chunyang.xu
+* @Last Modified time: 2020-12-14 18:04:57
+*/
+
+
+package main
+import (
+	"fmt"
+	"regexp"
+	"strconv"
+)
+func main() {
+	//目标字符串
+	searchIn := "John: 2578.34 William: 4567.23 Steve: 5632.18 test 0.02"
+	pat := "[0-9]+.[0-9]+" //正则
+
+	f := func(s string) string {
+    	v, _ := strconv.ParseFloat(s, 32)
+    	return strconv.FormatFloat(v * 2, 'f', 2, 32)
+	}
+
+	if ok, _ := regexp.Match(pat, []byte(searchIn)); ok {
+    	fmt.Println("Match Found!")
+	}
+
+	re, _ := regexp.Compile(pat)
+	//将匹配到的部分替换为"##.#"
+	str := re.ReplaceAllString(searchIn, "##.#")
+	fmt.Println(str)
+	//参数为函数时
+	str2 := re.ReplaceAllStringFunc(searchIn, f)
+	fmt.Println(str2)
+}
