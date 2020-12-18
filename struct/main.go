0ce6ef8a0be48c8657bfe02f59b5b2d526dd2497
longfengpili <398745129@qq.com>
2020-12-18 10:36:54 +0800

    [新增]interface

--- struct/main.go
+++ struct/main.go
@@ -2,7 +2,7 @@
 * @Author: chunyang.xu
 * @Date:   2020-12-16 11:31:53
 * @Last Modified by:   chunyang.xu
-* @Last Modified time: 2020-12-16 14:41:35
+* @Last Modified time: 2020-12-17 18:30:20
 */
 
 package main
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	struct1 := new(structPack.ExpStruct)
+	struct1 := new(struct_test.ExpStruct)
 	struct1.Mi1 = 10
 	struct1.Mf1 = 16.0
 	fmt.Println(struct1)
--- struct/struct_pack/structPack2.go
+++ struct/struct_pack/structPack2.go
@@ -2,12 +2,12 @@
 * @Author: chunyang.xu
 * @Date:   2020-12-16 11:30:47
 * @Last Modified by:   chunyang.xu
-* @Last Modified time: 2020-12-17 18:29:37
+* @Last Modified time: 2020-12-17 18:29:48
 */
 
 package struct_test
 
-type ExpStruct struct {
+type ExpStruct2 struct {
 	Mi1 int
 	Mf1 float32
 }
