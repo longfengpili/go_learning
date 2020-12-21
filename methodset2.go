89ea9da85bca909c77b871c9701c0ba89b2edf3b
longfengpili <398745129@qq.com>
2020-12-21 10:09:57 +0800

    [新增]methodset2

--- methodset2.go
+++ methodset2.go
@@ -2,7 +2,7 @@
 * @Author: chunyang.xu
 * @Date:   2020-12-18 10:37:19
 * @Last Modified by:   chunyang.xu
-* @Last Modified time: 2020-12-18 10:43:28
+* @Last Modified time: 2020-12-18 11:30:35
 */
 
 package main
@@ -16,10 +16,10 @@ func (l List) Len() int {
 }
 
 func (l *List) Append(val int) {
-	l = append(l, val)
+	*l = append(*l, val)
 }
 
-type Appender struct {
+type Appender interface {
 	Append(int)
 }
 
@@ -30,7 +30,7 @@ func CountInto(a Appender, start, end int) {
 }
 
 type lener interface {
-	len() int
+	Len() int
 }
 
 func LongEnough(l lener) bool {
@@ -39,13 +39,20 @@ func LongEnough(l lener) bool {
 
 func main() {
 	var lst List
+	// CountInto(lst, 1, 10)  # error
 	if LongEnough(lst) {
 		fmt.Printf("- lst is not enough \n")
+	} else {
+		fmt.Printf("- lst is : %v\n", lst)
 	}
 
 	plst := new(List)
 	CountInto(plst, 1, 10)
-	if LongEnough(lst) {
+	fmt.Println(plst)
+	if LongEnough(plst) {
 		fmt.Printf("- plst is long enough \n")
 	}
 }
+
+// 在 lst 上调用 CountInto 时会导致一个编译器错误，因为 CountInto 需要一个 Appender，而它的方法 Append 只定义在指针上。 在 lst 上调用 LongEnough 是可以的，因为 Len 定义在值上。
+// 在 plst 上调用 CountInto 是可以的，因为 CountInto 需要一个 Appender，并且它的方法 Append 定义在指针上。 在 plst 上调用 LongEnough 也是可以的，因为指针会被自动解引用。
--- test.go
+++ test.go
@@ -2,39 +2,23 @@
 * @Author: chunyang.xu
 * @Date:   2020-12-15 15:27:37
 * @Last Modified by:   chunyang.xu
-* @Last Modified time: 2020-12-15 15:51:46
+* @Last Modified time: 2020-12-18 11:55:28
 */
 
 package main
 import (
 	"fmt"
-	"strings"
+	// "strings"
 )
 
-type Person struct {
-	firstName string
-	lastName string
-}
+type Intlist []int
 
-func upPerson(p Person) {
-	p.firstName = strings.ToUpper(p.firstName)
-	p.lastName = strings.ToUpper(p.lastName)
+func (l Intlist) Len() int {
+	return len(l)
 }
 
 func main() {
-	var pers1 Person
-	pers1.firstName = "Chris"
-	pers1.lastName = "Woodward"
-	upPerson(pers1)
-	fmt.Println(pers1)
-
-	pers2 := new(Person)
-	pers2.firstName = "Chris"
-	pers2.lastName = "Woodward"
-	upPerson(*pers2)
-	fmt.Println(pers2)
-
-	pers3 := &Person{"Chris", "Woodward"}
-	upPerson(*pers3)
-	fmt.Println(pers3)
+	data := []int{1, 4, 3, 2}
+	il := Intlist(data)
+	fmt.Println(il)
 }
